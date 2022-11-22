b7216664b37a585c396ca8618a966bee3b9e58fe
Viktor <h048686@@stud.u-szeged.hu>
2022-11-22 20:54:21 +0100

    model2 is implemented more or less (it works)

---
+++ src/test2.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from src.dataloader import DataLoader
+from src.model2 import RostModelHungary
+from src.r0 import R0Generator
+import xlrd
+
+
+def main():
+    age_data_file = "C:/Users/Viktor/PycharmProjects/covid19-country-clustering/data/age_data.xlsx"
+    wb = xlrd.open_workbook(age_data_file)
+    sheet = wb.sheet_by_index(0)
+    datalist = np.array([sheet.row_values(i)[1:] for i in range(1, sheet.nrows)])
+    wb.unload_sheet(0)
+    age_data = datalist
+    print(age_data)
+    print(age_data.shape[1])
+    dicti = {"key1": [1, 2, 3, 4], "key2": [1, 2, 3, 4], "key3": [1, 2, 3, 4], "key4": [1, 2, 3, 4]}
+    array = list(dicti.values())
+    arr = np.array(array).flatten()
+    print(arr)
+    print(dicti["key1"])
+    l = [dicti[keys] for keys in dicti.keys()]
+    a = np.array(l).flatten()
+    print(a)
+
+
+def main2():
+    data = DataLoader()
+    model = RostModelHungary(model_data=data)
+    full_contact_mx = np.sum([data.contact_data["Hungary"][i] for i in data.contact_data["Hungary"].keys()], axis=0)
+    data.model_parameters_data.update({"beta": 0.1})
+    data.model_parameters_data.update({"susc": np.ones(16) * 0.5})
+    sol = model.get_solution(t=model.time_vector, parameters=data.model_parameters_data, cm=full_contact_mx)
+    deaths = model.get_deaths(solution=sol)
+    plt.plot(model.time_vector, deaths)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main2()
