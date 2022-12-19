6f08ec1a43387aa0d6ef9a723ab081b8a0a8fb60
Viktor <h048686@@stud.u-szeged.hu>
2022-12-19 17:25:45 +0100

    method death_beta_0 added

--- src2/beta0.py
+++ src2/beta0.py
@@ -6,18 +6,21 @@ import numpy as np
 
 
 class TransmissionRateCalc:
-    def __init__(self, data: DataLoader, country: str, concept: str, base_r0: float = 1):
+    def __init__(self, data: DataLoader, country: str, concept: str, base_r0: float = 1, final_death_rate: float = 0.001):
         self.data = data
         self.country = country
         self.concept = concept
         self.base_r0 = base_r0
+        self.final_death_rate = final_death_rate
 
         self.contact_mtx = self.get_contact_mtx()
 
     def run(self):
         r0generator = R0Generator(param=self.data.model_parameters_data)
-        if self.concept == "R0":
+        if self.concept == "base_r0":
             return self.base_r0 / r0generator.get_eig_val(contact_mtx=self.contact_mtx)
+        elif self.concept == "final_death_rate":
+            return self.death_beta_0()
 
     def get_contact_mtx(self):
         contact_home = self.data.contact_data[self.country]["home"]
@@ -26,3 +29,26 @@ class TransmissionRateCalc:
         contact_other = self.data.contact_data[self.country]["other"]
         contact_matrix = contact_home + contact_school + contact_work + contact_other
         return contact_matrix
+
+    def death_beta_0(self):
+        model = RostModelHungary(model_data=self.data, country=self.country)
+        self.data.model_parameters_data.update({"susc": np.ones(16) * 0.2})
+        betas = np.zeros(100)
+        deaths = np.zeros(100)
+        dicti = dict()
+        i = 0
+        for beta in np.arange(0.01, 1.01, 0.01):
+            betas[i] = beta
+            self.data.model_parameters_data.update({"beta": beta})
+            sol = model.get_solution(t=model.time_vector, parameters=self.data.model_parameters_data,
+                                     cm=self.contact_mtx)
+            deaths[i] = np.sum(model.get_deaths(solution=sol))
+            i += 1
+            dicti.update({deaths[i]: i})
+        deaths_min = min(abs(deaths - self.final_death_rate * np.sum(model.population)))
+        beta = 0
+        for k in range(len(deaths)):
+            if deaths[k] == self.final_death_rate * np.sum(model.population) - deaths_min \
+                    or deaths[k] == deaths_min + self.final_death_rate * np.sum(model.population):
+                beta = round(betas[k], 3)
+        return beta
