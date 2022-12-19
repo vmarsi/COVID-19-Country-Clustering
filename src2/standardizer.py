9bf386d817ecf403949cac1331a4416afe0b640e
Viktor <h048686@@stud.u-szeged.hu>
2022-12-19 22:27:38 +0100

    it can now handle all countries at once

--- src2/standardizer.py
+++ src2/standardizer.py
@@ -1,6 +1,8 @@
 from src2.beta0 import TransmissionRateCalc
 from src2.dataloader import DataLoader
 
+import numpy as np
+
 
 class Standardizer:
     def __init__(self, dl: DataLoader, concept: str, base_r0: float = 1.4, final_death_rate: float = 0.001):
@@ -8,18 +10,26 @@ class Standardizer:
         self.concept = concept
         self.base_r0 = base_r0
         self.final_death_rate = final_death_rate
-        self.stand_mtx = []
+        self.stand_mtxs = np.zeros((39, 16, 16))
 
     def run(self):
-        beta0 = TransmissionRateCalc(data=self.dl, country="Hungary", concept=self.concept, base_r0=self.base_r0,
-                                     final_death_rate=self.final_death_rate)
-        self.stand_mtx = beta0.run() * beta0.contact_mtx
-        print(beta0.run(), beta0.contact_mtx, self.stand_mtx)
+        self.dl.model_parameters_data.update({"calculated_beta_0": np.zeros(39)})
+        i = 0
+        for country in self.dl.contact_data.keys():
+            beta0 = TransmissionRateCalc(data=self.dl, country=country, concept=self.concept, base_r0=self.base_r0,
+                                         final_death_rate=self.final_death_rate)
+            beta_calc = beta0.run()
+            stand_mtx = beta_calc * beta0.contact_mtx
+            self.dl.model_parameters_data["calculated_beta_0"][i] = beta_calc
+            self.stand_mtxs[i] = stand_mtx
+            i += 1
+            print(i)
+        print(self.dl.model_parameters_data["calculated_beta_0"])
 
 
 def main():
     dl = DataLoader()
-    standardizer = Standardizer(dl=dl, concept="final_death_rate", final_death_rate=0.001)
+    standardizer = Standardizer(dl=dl, concept="final_death_rate", base_r0=1.4)
     standardizer.run()
 
 
