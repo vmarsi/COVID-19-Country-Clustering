cd3f7e25f7d8cb2b099c4dddf8898e7ce556b6e7
Viktor <h048686@@stud.u-szeged.hu>
2023-01-13 21:53:17 +0100

    manual runs added

--- src2/main_run.py
+++ src2/main_run.py
@@ -7,7 +7,9 @@ from src2.standardizer import Standardizer
 def main():
     dl = DataLoader()
     standardizer = Standardizer(dl=dl, concept="base_r0", base_r0=1.4)
+    standardizer.run()
     dimred = DimRed(stand=standardizer, dim_red="PCA")
+    dimred.run()
     clustering = Clustering(dimred=dimred, img_prefix="pca_", threshold=0.25)
     clustering.run()
 
