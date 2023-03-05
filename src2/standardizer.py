4caffb6a818e311576914f9570364d5107a58e73
Viktor <h048686@@stud.u-szeged.hu>
2023-03-05 20:43:03 +0100

    calc_r0 added ot data_all_dict

--- src2/standardizer.py
+++ src2/standardizer.py
@@ -1,11 +1,12 @@
 from src2.beta0 import TransmissionRateCalc
 from src2.dataloader import DataLoader
+from src2.r0 import R0Generator
 
 import numpy as np
 
 
 class Standardizer:
-    def __init__(self, dl: DataLoader, concept: str, base_r0: float = 1.4, final_death_rate: float = 0.001):
+    def __init__(self, dl: DataLoader, concept: str, base_r0: float = 2.2, final_death_rate: float = 0.001):
         self.dl = dl
         self.concept = concept
         self.base_r0 = base_r0
@@ -14,19 +15,18 @@ class Standardizer:
         self.data_all_dict = dict()
 
     def run(self):
-        self.dl.model_parameters_data.update({"calculated_beta_0": np.zeros(39)})
-        i = 0
+        r0generator = R0Generator(param=self.dl.model_parameters_data)
         stand_mtxs_temp = []
         for country in self.dl.contact_data.keys():
             beta0 = TransmissionRateCalc(data=self.dl, country=country, concept=self.concept, base_r0=self.base_r0,
                                          final_death_rate=self.final_death_rate)
             beta_calc = beta0.run()
             stand_mtx = beta_calc * beta0.contact_mtx
-            self.dl.model_parameters_data["calculated_beta_0"][i] = beta_calc
             stand_mtxs_temp.append(stand_mtx)
             self.data_all_dict.update(
                 {country: {"beta": beta_calc,
                            "contact_full": beta0.contact_mtx,
+                           "calc_r0": beta_calc * r0generator.get_eig_val(contact_mtx=beta0.contact_mtx),
                            "contact_home": self.dl.contact_data[country]["home"],
                            "contact_school": self.dl.contact_data[country]["school"],
                            "contact_work": self.dl.contact_data[country]["work"],
