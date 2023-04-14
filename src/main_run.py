1b5866046895598f240e6eb5e090572d9e3100dd
Viktor <h048686@@stud.u-szeged.hu>
2023-04-14 22:24:12 +0200

    nothing changed

--- src/main_run.py
+++ src/main_run.py
@@ -6,11 +6,11 @@ from src.standardizer import Standardizer
 
 def main():
     dl = DataLoader()
-    standardizer = Standardizer(dl=dl, concept="base_r0", base_r0=2.2)
+    standardizer = Standardizer(dl=dl, concept="base_r0")
     standardizer.run()
     dimred = DimRed(stand=standardizer, dim_red="2D2PCA")
     dimred.run()
-    clustering = Clustering(dimred=dimred, img_prefix="2dpca_", threshold=4)
+    clustering = Clustering(dimred=dimred, img_prefix="2d2pca_", threshold=7)
     clustering.run()
 
 
