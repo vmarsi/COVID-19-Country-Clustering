e36e8f3343a5056d1cc1d32f9d3c97a35e6e4ab2
Viktor <h048686@@stud.u-szeged.hu>
2023-05-04 14:53:31 +0200

    main is more precise

--- src/main_run.py
+++ src/main_run.py
@@ -6,11 +6,11 @@ from src.standardizer import Standardizer
 
 def main():
     dl = DataLoader()
-    standardizer = Standardizer(dl=dl, concept="base_r0")
+    standardizer = Standardizer(dl=dl, concept="final_death_rate")
     standardizer.run()
     dimred = DimRed(stand=standardizer, dim_red="2D2PCA")
     dimred.run()
-    clustering = Clustering(dimred=dimred, img_prefix="2d2pca_", threshold=7)
+    clustering = Clustering(dimred=dimred, img_prefix="2d2pca_fdr_man_", threshold=15, dist="manhattan")
     clustering.run()
 
 
